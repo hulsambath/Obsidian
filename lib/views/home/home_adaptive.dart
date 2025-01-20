8c9013ace7edd2d462d18f65a0d51aac3350a181
hulsambath <hulsambath14@gmail.com>
2025-01-20 17:51:08 +0700

    Close #56 config filledbutton to be the same as UI designed

--- lib/views/home/home_adaptive.dart
+++ lib/views/home/home_adaptive.dart
@@ -18,16 +18,6 @@ class _HomeAdaptiveState extends State<HomeAdaptive> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              tr('label.home'),
-              style: TextStyle(color: ColorScheme.of(context).onPrimary),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const SizedBox(
-              height: 12,
-            ),
             FilledButton.icon(
               onPressed: () {
                 setState(() {
@@ -38,24 +28,6 @@ class _HomeAdaptiveState extends State<HomeAdaptive> {
               label: dark ? const Text('Dark Mode') : const Text('Light Mode'),
               icon: dark ? const Icon(Icons.dark_mode) : const Icon(Icons.light_mode),
             ),
-            const SizedBox(
-              height: 12,
-            ),
-            FilledButton(
-              onPressed: () {
-                context.setLocale(const Locale('en'));
-              },
-              child: const Text('English'),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            FilledButton(
-              onPressed: () {
-                context.setLocale(const Locale('km'));
-              },
-              child: const Text('Khmer'),
-            )
           ],
         ),
       ),
--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -2,7 +2,6 @@ library home_screen;
 
 import 'package:auto_route/auto_route.dart';
 import 'package:cm_vendor_app/providers/theme_provider.dart';
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
